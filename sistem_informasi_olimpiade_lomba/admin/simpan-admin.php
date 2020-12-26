2e946285d8a1ad07a916d6a26a593bb8c643c065
Developer Aljannah <76476702+developeraljannah@users.noreply.github.com>
2020-12-26 07:43:07 +0700

    membuat register dan simpan admin

---
+++ sistem_informasi_olimpiade_lomba/admin/simpan-admin.php
@@ -0,0 +1,13 @@
+<?php
+
+// koneksi database
+include '../koneksi.php';
+
+// menangkap data yang di kirim dari form
+$username = mysqli_real_escape_string($koneksi, $_POST['user']);
+$password = mysqli_real_escape_string($koneksi, $_POST['pass']);
+$status = "pending";
+
+// menginput data ke database
+mysqli_query($koneksi, "INSERT INTO tb_login(username, password, status) VALUES ('$username','$password','$status')");
+echo "<script>alert('Akun sedang dalam masa verifikasi, terimakasih untuk pendaftarannya');window.location='login.php'</script>";
