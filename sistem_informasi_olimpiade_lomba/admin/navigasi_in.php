5a8a9e7132e509070696f7002611e959269bd080
Developer Aljannah <76476702+developeraljannah@users.noreply.github.com>
2021-01-08 06:43:43 +0700

    Update navigasi_in.php

--- sistem_informasi_olimpiade_lomba/admin/navigasi_in.php
+++ sistem_informasi_olimpiade_lomba/admin/navigasi_in.php
@@ -6,7 +6,7 @@
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Home
                             </a>
-                            <a class="nav-link" href="index.html">
+                            <a class="nav-link" href="../data_gp/lihat_gp.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Data Guru Pendamping
                             </a>
@@ -14,22 +14,21 @@
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Data Juri
                             </a>
-                            <a class="nav-link" href="index.html">
+                            <a class="nav-link" href="../data_kelas/lihat_kelas.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Data Kelas
                             </a>
-                            <a class="nav-link" href="index.html">
+                            <a class="nav-link" href="../data_kompetisi/lihat_kompetisi.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Data Kompetisi
                             </a>
-                            <a class="nav-link" href="index.html">
+                            <a class="nav-link" href="../data_nilai/lihat_nilai.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Data Nilai
                             </a>
-                            <a class="nav-link" href="index.html">
+                            <a class="nav-link" href="../data_siswa/lihat_siswa.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Data Siswa
                             </a>
                         </div>
                     </div>
-                    
